93906c4c8335d84bbeee1ab0f1a2c4a1c606b059
Aditya Kumar Jha <aditya.ibm.kumar@gmail.com>
2025-04-28 11:40:03 +0530

    studentapp

--- frontend/adi/src/component/Delete.jsx
+++ frontend/adi/src/component/Delete.jsx
@@ -5,7 +5,7 @@ function Delete() {
     const adi=async(e)=>{
         e.preventDefault
         const id=e.target.id.value;
-        await axios.delete(`http://localhost:9000/users/${id}`)
+        await axios.delete(`https://studentapp1-w12l.onrender.com/${id}`)
         alert("data delete")
     }
   return (
